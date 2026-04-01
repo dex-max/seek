23ae83eb92d2406714ce7068a496635f34ec30cc
Randall <randall.eagan.kim@gmail.com>
2026-04-01 18:45:49 -0500

    Add /videos route

--- seek.py
+++ seek.py
@@ -1,11 +1,25 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
 from flask import Flask, render_template
 
+load_dotenv()
+
 app = Flask(__name__)
+VIDEO_FOLDER = Path(os.getenv("VIDEO_FOLDER", "."))
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
-if __name__ == "__main__":
-    app.run()
+@app.route("/videos")
+def videos():
+    videos = []
+
+    for file in VIDEO_FOLDER.iterdir():
+        videos.append({"title": file.name})
+
+    return videos
